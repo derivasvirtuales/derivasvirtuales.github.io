ce667a5a02b2a78fc1e814f249d4052b6387220e
NicMotta <34142261+NicMotta@users.noreply.github.com>
2021-07-20 22:57:37 -0300

    edificios movidos

--- info-edificios.js
+++ info-edificios.js
@@ -13,7 +13,7 @@ const info = {
     texto: `Se sitúa en el edificio que funcionó como Pabellón de Armas y Aviación, y 
     luego como Escuela de Guerra Naval, dentro del predio de la Ex-Esma. La ESMA fue 
     uno de los centros de detención, tortura y exterminio más importantes que implementó 
-    la última dictadura cívico militar en la Argentina entre 1976 y 1983. En este espacio, 
+    la última dictadura <i>cívico militar</i> en la Argentina entre 1976 y 1983. En este espacio, 
     se realizan diversas actividades culturales en torno a la historia, la memoria, 
     los derechos humanos y el arte para resignificar este lugar marcado por el terrorismo de 
     estado. El CCMHC posee una colección permanente de obras de artes visuales y también 
@@ -70,22 +70,13 @@ const info = {
     nombre: 'Fundación PROA',
     lugar: {
       barrio: 'Barrio',
-      ubicacion: 'Palermo'
+      ubicacion: 'La Boca'
     },
     imagen: {
-      src: "./assets/img/fundacion-proa.jpg",
+      src: "./assets/img/museos/Fundacion Proa.JPG", 
       autor: `Mariel Szlifman`
     },
-    texto: `Se sitúa en el edificio que funcionó como Pabellón de Armas y Aviación, y 
-    luego como Escuela de Guerra Naval, dentro del predio de la Ex-Esma. La ESMA fue 
-    uno de los centros de detención, tortura y exterminio más importantes que implementó 
-    la última dictadura cívico militar en la Argentina entre 1976 y 1983. En este espacio, 
-    se realizan diversas actividades culturales en torno a la historia, la memoria, 
-    los derechos humanos y el arte para resignificar este lugar marcado por el terrorismo de 
-    estado. El CCMHC posee una colección permanente de obras de artes visuales y también 
-    siete salas para exposiciones temporales exhibidas en el área “Espacio de Arte Federico Venturi”. 
-    La curaduría de las muestras temporales reúne diferentes campos artísticos, en exposiciones que 
-    potencian la memoria del espacio desde el presente de modo simbólico, articulando el contenido con el contexto expositivo y su identidad.`,
+    texto: `Espacio de arte privado fundado en 1996 y emplazado en el barrio de La Boca, perteneciente a la empresa Tenaris-Techint. No posee colección propia y su programa artístico se basa en exposiciones temporales locales e internacionales de corrientes artísticas modernas y contemporáneas. Está emplazada en una antigua casona de estilo italiano del siglo XIX, refuncionalizada a modo de «open space» en el que conviven salas expositivas, auditorio, librería y café. Su arquitectura se caracteriza por la transparencia espacial que habilita un vínculo entre su interior y exterior. En el proceso de reforma y acondicionamiento, se rescataron materialidades (concreto-madera) y usos de su arquitectura anterior para mantener una memoria edilicia y para establecer una relación con su contexto urbano. A su vez, en una segunda reforma y ampliación, hubo un rediseño de su marca conforme a procesos de «<i>branding cultural</i>» y se apostó por un diseño de identidad que toma como inspiración tipográfica los remaches (y el estilo portuario) del Puente Transbordador Nicolás Avellaneda. De esta manera, se generó un diálogo estético y cultural con este símbolo del barrio y se reforzó la identidad de marca.`,
   },
   'MUNTREFArtesVisuales': {
     categoria: 'Contemporáneo',
@@ -108,6 +99,261 @@ const info = {
     siete salas para exposiciones temporales exhibidas en el área “Espacio de Arte Federico Venturi”. 
     La curaduría de las muestras temporales reúne diferentes campos artísticos, en exposiciones que 
     potencian la memoria del espacio desde el presente de modo simbólico, articulando el contenido con el contexto expositivo y su identidad.`,
-  }
+  },
+
+  //   //   // ////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////
+
+  'MALBA': {
+    categoria: 'Contemporáneo',
+    nombre: 'Museo de Arte Latinoamericano de Buenos Aires',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: 'Palermo'
+    },
+    imagen: {
+      src: "./assets/img/museos/Malba.JPG",
+      autor: `Ernesto Monasterio`
+    },
+    texto: `Museo privado creado en el 2001 para albergar la colección de arte latinoamericano de Eduardo Constantini. 
+    Es uno de los pocos museos nacionales que, desde sus orígenes, 
+    fue concebido y construido específicamente con este fin. 
+    Su configuración arquitectónica es una caja multifuncional 
+    (exhibición, experiencia, consumo y entretenimiento) 
+    que está orientada a una ideología de acceso directo, 
+    transparencia y funcionalidad y que ofrece espacios flexibles para el arte y una propuesta de visión vinculada con la idea de mirada flotante 
+    (ausencia de recorrido dirigido). Es un «museo de fin de siglo» que, desde su museografía, 
+    logra posicionarse a nivel internacional e interactuar con un público contemporáneo. 
+    A su vez, posee una fuerte impronta como marca en torno a procesos de «<i>branding cultural</i>». 
+    La marca Malba articula aspectos históricos, 
+    culturales y sociales alrededor del mito de identidad de la institución como hito cultural (Buenos Aires como epicentro). 
+    Su emplazamiento urbano es Palermo Chico/Barrio Parque (una especie de barrio cerrado en el medio de la ciudad) y linda con el <i>shopping</i> Paseo Alcorta.`,
+  },
+
+  'FundacionFortabat': {
+    categoria: 'Contemporáneo',
+    nombre: 'Colección de Arte Amalia Lacroze de Fortabat',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: 'Puerto Madero'
+    },
+    imagen: {
+      src: "./assets/img/museos/Coleccion de Arte Amalia Lacroze de Fortabat.JPG",
+      autor: `Mariel Szlifman`
+    },
+    texto: `Espacio de arte perteneciente a una antigua familia de empresarios, dueños de la compañía de cemento Loma Negra, emplazado en el contexto urbano de Puerto Madero. El proyecto nace a partir de una iniciativa privada en torno a la colección personal y se configuró como una «arquitectura de autor», en una alianza entre el arquitecto uruguayo Rafael Viñoly y la coleccionista-mecenas Amalia Fortabat. El objetivo era construir un espacio que se posicione como ícono dentro de la escena mediática local e internacional. Se planteó, así, un nuevo discurso e identidad para la colección, junto con una nueva forma de exhibirla. El espacio fue especialmente diseñado y construido para este fin. La arquitectura responde a criterios propios del contexto en que se emplaza, respetando la altura de la arquitectura inglesa de la zona, pero incorporando materialidades contemporáneas como acero y vidrio. A su vez, la configuración arquitectónica afecta el diseño expositivo, ya que, al momento de su construcción, no hubo una política museográfica, más allá de la colección. Esto da como resultado un espacio arquitectónico que se va transformando para poder albergar diferentes muestras temporales.`,
+  },
+
+  'CasaRosada': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'CCK': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'CCR': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'Conti': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'CornelioSaavedra': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'EspacioTelefonica': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'FADU': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'FOLA': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'MAMBA': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'MUNTREF': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'OSDE': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'ParqueMemoria': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'RolfArt': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'SalaPays': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+  'SanMartin': {
+    categoria: '',
+    nombre: '',
+    lugar: {
+      barrio: 'Barrio',
+      ubicacion: ''
+    },
+    imagen: {
+      src: "./assets/img/museos/",
+      autor: ``
+    },
+    texto: ``,
+  },
+
+
 }
 
